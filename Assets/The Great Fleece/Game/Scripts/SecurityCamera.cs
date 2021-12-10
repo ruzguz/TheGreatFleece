befd70eaf851daf43a0faa33ca39a129c0cd2a64
Rusben Guzman <rusbenguzman@gmail.com>
2021-12-10 11:27:59 -0500

    Change camera cone color when collides with the player

--- Assets/The Great Fleece/Game/Scripts/SecurityCamera.cs
+++ Assets/The Great Fleece/Game/Scripts/SecurityCamera.cs
@@ -4,14 +4,42 @@ using UnityEngine;
 
 public class SecurityCamera : MonoBehaviour
 {
+
+    private Animator _animator;
     [SerializeField] 
     private GameObject _gameOverCutscene;
+    [SerializeField]
+    private MeshRenderer _coneRenderer;
+
+    private void Start() 
+    {
+        _animator =  GetComponent<Animator>();
+        if (_animator == null) 
+        {
+            Debug.LogError("Animator is NULL");
+        }
+
+        _coneRenderer = GetComponentsInChildren<MeshRenderer>()[1];
+        if (_coneRenderer == null) 
+        {
+            Debug.LogError("Mesh Renderer is NULL");
+        }
+    }
 
     private void OnTriggerEnter(Collider other) {
         
         if (other.CompareTag("Player")) 
         {
-            _gameOverCutscene.SetActive(true);
+            StartCoroutine(ActiveCutscene());
         }
+    }
+
+    IEnumerator ActiveCutscene()
+    {
+        Color color = new Color(0.6f,0.1f,0.1f,0.3f);
+        _coneRenderer.material.SetColor("_TintColor", color);
+        _animator.enabled = false;
+        yield return new WaitForSeconds(3.0f);
+        _gameOverCutscene.SetActive(true);
     }
 }
