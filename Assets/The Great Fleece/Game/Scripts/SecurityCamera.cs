57fb65bc6bd65f06020049e2d01cb7ef876e3ac6
Rusben Guzman <rusbenguzman@gmail.com>
2021-12-14 17:03:39 -0500

    Added interaction with sleeping guard

--- Assets/The Great Fleece/Game/Scripts/SecurityCamera.cs
+++ Assets/The Great Fleece/Game/Scripts/SecurityCamera.cs
@@ -39,7 +39,7 @@ public class SecurityCamera : MonoBehaviour
         Color color = new Color(0.6f,0.1f,0.1f,0.3f);
         _coneRenderer.material.SetColor("_TintColor", color);
         _animator.enabled = false;
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(1.0f);
         _gameOverCutscene.SetActive(true);
     }
 }
