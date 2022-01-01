dbe011740cbea116628f25e115e0d462fec85618
Rusben Guzman <rusbenguzman@gmail.com>
2022-01-01 09:42:02 -0500

    Created UI Manager, Restart function and Quit Function

---
+++ Assets/The Great Fleece/Game/Scripts/UIManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+    private static UIManager _instance;
+
+    public static UIManager Instance 
+    {
+        get 
+        {
+            if (_instance == null) 
+            {
+                Debug.LogError("UIManager is null");
+            }
+            return _instance;
+        }
+    }
+
+
+    void Awake() 
+    {
+        if (_instance == null) 
+        {
+            _instance = this;
+        }
+    }
+
+    public void Quit() 
+    {
+        //Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void Restart()
+    { 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
