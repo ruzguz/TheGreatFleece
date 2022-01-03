2878839dbc3fa7d50cbdce171fb85c213f7e54dc
Rusben Guzman <rusbenguzman@gmail.com>
2022-01-03 10:28:52 -0500

    Added start game and quit funtionalities in main meni

---
+++ Assets/The Great Fleece/Game/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void StartGame() 
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Quit()
+    {
+        //Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+}
